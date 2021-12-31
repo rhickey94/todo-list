d0aba3f054b4e1659fef279f5ace1e1e4734029b
Ryan <rhickey94@gmail.com>
2021-12-30 22:50:09 -0500

    initial header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,1 +1,48 @@
-console.log("sup")
+function header() {
+  const head = document.head;
+  const style = createStyle()
+  head.appendChild(style);
+  
+  const header = document.createElement("header");
+  header.setAttribute("id", "header");
+  
+  const div = document.createElement("div");
+  div.setAttribute("id", "logo");
+  div.textContent = "To Do!";
+  
+  header.appendChild(div);
+  
+  return header;
+}
+
+function createStyle() {
+  const style = document.createElement("style");
+  style.appendChild(headerStyling());
+  
+  return style;
+}
+
+function headerStyling() {
+  const css = `
+
+    #header { 
+      color: #F1E3F3;
+      background-color: #211A1D;
+
+      font-size: 36px;
+      font-weight: lighter;
+      height: 10%;
+
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+
+      padding-left: 10%;
+    }
+    
+    `;
+
+  return document.createTextNode(css);
+}
+
+export default header;
